ad2ad539e54e023eb3db7322fae05bec1e2f90c2
Duke <dukemorise33@gmail.com>
2023-12-23 23:09:18 -0800

    add new task on Schedule class works

--- lib/debug.py
+++ lib/debug.py
@@ -9,8 +9,7 @@ def testing_create_table():
     schedule = Schedule.create("Art Class")
     Task.create("12/21/2023","10:00am",2,"Meeting with Steve the art class TA", schedule.id)
     Task.create("12/25/2023", "12:00am", 0.5, "Art convention meeting with Santa", schedule.id)
-
-    
+    schedule.add_new_task("01/09/2024", "09:00am", 3, "Orientation for Art Program")
 
 
 testing_create_table()
